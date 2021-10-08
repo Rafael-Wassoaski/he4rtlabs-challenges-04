d7c86b6bd5a88f381b5fb1156d07c263b233e3b8
Rafael Wassoaski <meireles200@hotmail.com>
2021-10-07 21:32:12 -0300

    #time 10m
    Implementando banco de dados com modelos gerados pelo sequelize

--- src/api/user/userController.js
+++ src/api/user/userController.js
@@ -1,10 +1,13 @@
 const axios = require('axios').default;
+const dbConnection = require('../../db/dbInit');
+const sequelizeUser = require('../../db/models/User');
 
 class UserController{
     static async create(req, res, next){
         const{userName, password, email} = req.body;
         console.log('aqui',userName);
-
+        dbConnection;
+        await sequelizeUser();
         return res.send();
     }
 }
